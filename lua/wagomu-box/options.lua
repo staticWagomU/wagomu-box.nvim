6654c251457c8bc48edd7a15431cab283f878daa
staticWagomU <to6wa17@gmail.com>
2025-05-06 20:53:04 +0900

    update

--- lua/wagomu-box/options.lua
+++ lua/wagomu-box/options.lua
@@ -30,34 +30,36 @@ function M.apply()
     'start'
   }
   set.clipboard = 'unnamedplus,unnamed'
+	set.cmdheight = 0
   set.encoding = 'utf-8'
   set.expandtab = true
   set.fileencoding = 'utf-8'
   set.fileencodings = 'utf-8,euc-jp,cp932'
-  set.fillchars = {
-    stl = ' ',
-    stlnc = '─',
-    diff = '∙',
-    eob = ' ',
-    fold = '·',
-    horiz = '─',
-    horizup = '┴',
-    horizdown = '┬',
-    vert = '│',
-    vertleft = '┤',
-    vertright = '├',
-    verthoriz = '┼',
-  }
+	set.fillchars = {
+		stl = '─',
+		stlnc = '─',
+		diff = '∙',
+		eob = ' ',
+		fold = '·',
+		horiz = '─',
+		horizup = '┴',
+		horizdown = '┬',
+		vert = '│',
+		vertleft = '┤',
+		vertright = '├',
+		verthoriz = '┼',
+	}
   set.foldcolumn = '1'
   set.foldenable = true
   set.foldlevel = 99999
   set.foldlevelstart = 99999
+	set.foldtext = [[v:lua.vim.treesitter.foldtext()]]
   set.helplang = 'ja,en'
   set.hidden = true
   set.hlsearch = true
   set.ignorecase = true
   set.incsearch = true
-  set.laststatus = 3
+  set.laststatus = 0
   set.list = true
   set.listchars = {
     eol = '↴',
@@ -72,6 +74,7 @@ function M.apply()
   set.signcolumn = 'yes'
   set.smartcase = true
   set.softtabstop = 2
+	set.statusline = '─'
   set.tabstop = 2
   set.undofile = true
   set.undodir = vim.fn.stdpath('data') .. '/undo'
