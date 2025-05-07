5fe4f7937466410f162a48234b8f1bd2c6cfad62
staticWagomU <to6wa17@gmail.com>
2025-05-07 23:24:24 +0900

    feat: swapを消すやつ

--- lua/wagomu-box/commands.lua
+++ lua/wagomu-box/commands.lua
@@ -1,1 +1,14 @@
 vim.api.nvim_create_user_command('DenoCacheUpdate', 'call denops#cache#update(#{reload: v:true})', { nargs = 0 })
+
+vim.api.nvim_create_user_command("SwapClean", function()
+  local dirs = vim.opt.directory:get()
+  for _, dir in pairs(dirs) do
+    for name in vim.iter(vim.fs.dir(dir, { depth = 1 })):filter(function(_, type)
+      return type == "file"
+    end) do
+      local file = vim.fs.joinpath(dir, name)
+      vim.print("deleting " .. file)
+      vim.fs.rm(file)
+    end
+  end
+end, {})
