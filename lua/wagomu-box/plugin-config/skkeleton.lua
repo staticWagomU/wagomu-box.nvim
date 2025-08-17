23e0bba3cdf3a20bdbaddd25368272a005667f4e
staticWagomU <to6wa17@gmail.com>
2025-08-17 15:35:10 +0900

    feat(skkeleton): skkeleton入荷

---
+++ lua/wagomu-box/plugin-config/skkeleton.lua
@@ -0,0 +1,40 @@
+
+local M = {}
+
+local function setup(plugin_path)
+	local dictdir = vim.fn.expand(vim.fs.joinpath(plugins_path, 'dict'))
+	if ~vim.uv.fs_stat(dictdir) then
+		vim.notify_once('skk-dev/dictを入れてください')
+		return
+	end
+
+	local map, nmap = WagomuBox.map, WagomuBox.nmap
+
+	map({ 'i', 'c', 't' }, '<C-j>', '<Plug>(skkeleton-toggle)')
+	nmap('<C-j>', 'i<Plug>(skkeleton-toggle)')
+
+	vim.api.nvim_create_autocmd('User', {
+		pattern = 'skkeleton-initialize-pre',
+		callback = function()
+			local getJisyo = function(name)
+				local dictdir = vim.fn.expand(vim.fs.joinpath(plugins_path, 'dict', 'SKK-JISYO.'))
+				return vim.fs.normalize(dictdir .. name)
+			end
+			vim.fn['skkeleton#config'] {
+				eggLikeNewline = true,
+				globalDictionaries = {
+					getJisyo('L'),
+					getJisyo('hukugougo'),
+					getJisyo('mazegaki'),
+					getJisyo('propernoun'),
+					getJisyo('station'),
+				},
+				databasePath = '/tmp/skkeleton.sqlite3',
+			}
+			vim.fn['skkeleton#register_kanatable']('rom', {
+				[ [[z\<Space>]] ] = { [[\u3000]], '' },
+				[ [[xn]] ] = { [[ん]], '' },
+			})
+		end,
+	})
+end
